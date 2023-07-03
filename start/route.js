0c77bb61f2d8883ca8cb34235622f0944100dcfa
david00154 <davidbriggs00154@gmail.com>
2023-07-03 00:03:50 +0000

    initial commit

---
+++ start/route.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+app.use(require("morgan")("combined"));
+
+// middlewares;
+app.use(cors());
+// EJS
+app.use(express.static(path.join(__dirname, "static")));
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.set("views", path.join(process.cwd(), "views"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {});
+
+const port = process.env.PORT || 8080;
+
+try {
+  app.listen(port, () => {
+    console.log(`> Server started on port ${port}`);
+  });
+} catch (e) {
+  console.log(e);
+}
+
+module.exports = app;
